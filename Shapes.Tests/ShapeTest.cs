d4522810dd0f889d29685be75d971df24b1e6b7d
Andrey <androrvit@gmail.com>
2023-02-12 14:22:31 +0300

    add IsRectangularTest

--- Shapes.Tests/ShapeTest.cs
+++ Shapes.Tests/ShapeTest.cs
@@ -20,7 +20,23 @@ namespace Shapes.Tests
 				new object[] {new Circle(1.0), 3.1416 },
 				new object[] {new Circle(12.5), 490.8739 },
 			};
-			
+
+		[Theory]
+		[MemberData(nameof(IsRectangularTestData))]
+		public void IsRectangularTest(Triangle shape, bool isRectangular)
+		{
+			Assert.Equal(isRectangular, shape.IsRectangular());
+		}
+
+		public static IEnumerable<object[]> IsRectangularTestData =>
+			new List<object[]>
+			{
+				new object[] {new Triangle(1, 1, 1), false },
+				new object[] {new Triangle(3, 4, 5), true },
+				new object[] {new Triangle(5, 4, 3), true },
+				new object[] {new Triangle(6, 8, 10), true },
+				new object[] {new Triangle(5, 12, 13.0001), false },
+			};
 	}
 
 }
