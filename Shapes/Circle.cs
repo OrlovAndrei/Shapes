d32803a7100f21743f2af6e2f8fbee2c79188208
Andrey <androrvit@gmail.com>
2023-02-12 13:13:28 +0300

    add Circle

---
+++ Shapes/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+	public class Circle : IShape
+	{
+		public double Square => Math.PI * Math.Pow(_radius, 2);
+
+		private double _radius;
+
+		public Circle(double radius)
+		{
+			_radius= radius;
+		}
+	}
+}
