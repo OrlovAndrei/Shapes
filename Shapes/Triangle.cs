f74626ac1ebdd72812f77a5c91e17e47aa943f0d
Andrey <androrvit@gmail.com>
2023-02-12 14:39:32 +0300

    change Triangle

--- Shapes/Triangle.cs
+++ Shapes/Triangle.cs
@@ -1,4 +1,6 @@
-﻿namespace Shapes
+﻿using System.Data;
+
+namespace Shapes
 {
 	public class Triangle : IShape
 	{
@@ -6,11 +8,18 @@
 
 		private readonly double[] _sides = new double[3];
 
-		public Triangle(double side1, double side2, double side3)
+		public Triangle(double side1, double side2, double side3) : this(new[] { side1, side2, side3 }) { }
+
+		public Triangle(double[] sides)
 		{
-			_sides[0] = side1;
-			_sides[1] = side2;
-			_sides[2] = side3;
+			if (sides.Length != 3) throw new Exception("An array of the incorrect size was passed");
+
+			for (int i = 0; i < sides.Length; i++)
+				if (sides[i] > sides[(i + 1) % 3] + sides[(i + 2) % 3])
+					throw new Exception("A triangle with such sides is impossible");
+
+			for (int i = 0; i < sides.Length; i++)
+				_sides[i] = sides[i];
 		}
 		
 		public bool IsRectangular()
